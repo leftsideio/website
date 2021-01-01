e113e681b79e37a70590f66c9ae58d0dea095443
garretteklof <garrett@leftside.io>
2021-01-01 15:11:57 -0600

    delete styled-component theme provider

--- src/style/theme.js
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider } from "styled-components"
-
-const Provider = ({ children }) => <ThemeProvider theme={theme}>{children}</ThemeProvider>
-
-const theme = {
-  colors: {},
-}
-
-export { theme, Provider }
