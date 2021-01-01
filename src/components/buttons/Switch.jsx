4757fa7888eb8ba757e748f67c703a68586ea66e
garretteklof <garrett@leftside.io>
2021-01-01 15:40:57 -0600

    add sfx for switch

--- src/components/buttons/Switch.jsx
+++ src/components/buttons/Switch.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react"
 import styled from "styled-components"
 import { useTransition, animated } from "react-spring"
+import useSound from "use-sound"
 import { Icon as I } from "@/components/media"
+import onSfx from "#/sounds/switch-on.mp3"
+import offSfx from "#/sounds/switch-off.mp3"
 
 const Switch = ({ on, off, cb }) => {
   const [isSwitchOn, setSwitchOn] = useState(true) // todo: come from localStorage / external settings
+  const [playOn] = useSound(onSfx)
+  const [playOff] = useSound(offSfx)
   const transitions = useTransition(isSwitchOn, null, {
     config: {
       tension: 300,
@@ -15,14 +20,15 @@ const Switch = ({ on, off, cb }) => {
     leave: { opacity: 0, transform: "rotate(90deg)" },
   })
 
+  const onSwitchClick = () => {
+    const playSound = isSwitchOn ? playOff : playOn
+    playSound()
+    setSwitchOn(!isSwitchOn)
+    cb(!isSwitchOn)
+  }
+
   return (
-    <Box
-      onClick={() => {
-        const next = !isSwitchOn
-        setSwitchOn(next)
-        cb(next)
-      }}
-    >
+    <Box onClick={onSwitchClick}>
       {transitions.map(({ item, key, props }) => (
         <Icon key={key} name={item ? on.icon : off.icon} style={props} />
       ))}
