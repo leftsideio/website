97c713c7cd257ef01d435c7adef4d54d95babb99
garretteklof <garrett@leftside.io>
2021-04-11 01:16:34 -0500

    💅🏻 eslint-config-prettier v8

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   parser: `@typescript-eslint/parser`,
-  extends: ["plugin:@typescript-eslint/recommended", "prettier/@typescript-eslint", "plugin:prettier/recommended"],
-  plugins: ["@typescript-eslint", "prettier"],
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
